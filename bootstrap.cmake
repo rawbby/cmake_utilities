1b90676883e9329e3a74d25ce5e7fd17ec4aeb11
Robert Andreas Fritsch <git@robert-fritsch.de>
2025-01-20 14:52:37 +0100

    wip

--- bootstrap.cmake
+++ bootstrap.cmake
@@ -7,9 +7,9 @@ else ()
     set(CMAKE_UTILITIES_BASE_URL "https://raw.githubusercontent.com/rawbby/cmake_utilities/refs/tags/${CMAKE_UTILITIES_VERSION}")
 endif ()
 
-file(LOCK "${CMAKE_CURRENT_SOURCE_DIR}/.cmake_utilities/download.lock" GUARD FILE RESULT_VARIABLE LOCK_RESULT)
+set(LOCK_RESULT ON)
 if (LOCK_RESULT)
-    message(FATAL_ERROR "failed to acquire download.lock")
+    file(LOCK "${CMAKE_CURRENT_SOURCE_DIR}/.cmake_utilities/download.lock" GUARD FILE RESULT_VARIABLE LOCK_RESULT)
 endif ()
 
 if (NOT EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/.cmake_utilities/.valid")
