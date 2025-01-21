c6314fb51dbe1495a208c49db69198775720d923
Robert Andreas Fritsch <git@robert-fritsch.de>
2025-01-21 14:17:50 +0100

    wip

--- add_executables_directory.cmake
+++ add_executables_directory.cmake
@@ -16,7 +16,7 @@ function(add_executables_directory TARGETS_BASE_DIR)
     endif ()
 
     get_filename_component(TARGETS_ABS_DIR "${TARGETS_BASE_DIR}" ABSOLUTE)
-    abs_dirs_from_glob(TARGET_ABS_DIRS "${TARGETS_ABS_DIR}/**/target.cmake")
+    abs_dirs_from_glob(TARGET_ABS_DIRS "${TARGETS_ABS_DIR}/*/target.cmake")
 
     foreach (TARGET_ABS_DIR ${TARGET_ABS_DIRS})
         file(RELATIVE_PATH TARGET_REL_DIR "${TARGETS_ABS_DIR}" "${TARGET_ABS_DIR}")
