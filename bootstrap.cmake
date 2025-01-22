b482a437043fbf734866c709a7e9a96ea031c839
Robert Andreas Fritsch <git@robert-fritsch.de>
2025-01-22 16:57:56 +0100

    add online test

--- bootstrap.cmake
+++ bootstrap.cmake
@@ -1,36 +1,39 @@
 cmake_minimum_required(VERSION 3.14)
 
 set(CMAKE_UTILITIES_VERSION "main" CACHE STRING "The Version of CMakeUtilities to use")
-if (CMAKE_UTILITIES_VERSION STREQUAL "main")
-    set(CMAKE_UTILITIES_BASE_URL "https://raw.githubusercontent.com/rawbby/cmake_utilities/refs/heads/main")
-else ()
-    set(CMAKE_UTILITIES_BASE_URL "https://raw.githubusercontent.com/rawbby/cmake_utilities/refs/tags/${CMAKE_UTILITIES_VERSION}")
-endif ()
 
-set(LOCK_RESULT 1)
-while (LOCK_RESULT)
-    file(LOCK "${CMAKE_SOURCE_DIR}/.cmake_utilities/download.lock" GUARD FILE RESULT_VARIABLE LOCK_RESULT)
-endwhile ()
+block()
 
-if (NOT EXISTS "${CMAKE_SOURCE_DIR}/.cmake_utilities/.valid")
+    if (CMAKE_UTILITIES_VERSION STREQUAL "main")
+        set(BASE_URL "https://raw.githubusercontent.com/rawbby/cmake_utilities/refs/heads/main")
+    else ()
+        set(BASE_URL "https://raw.githubusercontent.com/rawbby/cmake_utilities/refs/tags/${CMAKE_UTILITIES_VERSION}")
+    endif ()
 
-    set(FILENAMES
-            "all.cmake"
-            "export.h.in"
-            "structured_directories.cmake")
+    set(LOCK_RESULT 1)
+    while (LOCK_RESULT)
+        file(LOCK "${CMAKE_SOURCE_DIR}/.cmake_utilities/download.lock" GUARD FILE RESULT_VARIABLE LOCK_RESULT)
+    endwhile ()
 
-    foreach (FILENAME ${FILENAMES})
-        file(DOWNLOAD
-                "${CMAKE_UTILITIES_BASE_URL}/.cmake_utilities/${FILENAME}"
-                "${CMAKE_SOURCE_DIR}/.cmake_utilities/${FILENAME}"
-                STATUS DOWNLOAD_STATUS)
-        list(GET DOWNLOAD_STATUS 0 DOWNLOAD_CODE)
-        list(GET DOWNLOAD_STATUS 1 DOWNLOAD_MESSAGE)
-        if (NOT ${DOWNLOAD_CODE} EQUAL 0)
-            message("${CMAKE_UTILITIES_BASE_URL}/.cmake_utilities/${FILENAME}")
-            message(FATAL_ERROR "File download failed with error code: ${DOWNLOAD_CODE} ${DOWNLOAD_MESSAGE}")
-        endif ()
-    endforeach ()
+    if (NOT EXISTS "${CMAKE_SOURCE_DIR}/.cmake_utilities/.valid")
+        set(FILENAMES
+                "all.cmake"
+                "export.h.in"
+                "structured_directories.cmake")
 
-    file(WRITE "${CMAKE_SOURCE_DIR}/.cmake_utilities/.valid" "")
-endif ()
+        foreach (FILENAME ${FILENAMES})
+            file(DOWNLOAD
+                    "${BASE_URL}/.cmake_utilities/${FILENAME}"
+                    "${CMAKE_SOURCE_DIR}/.cmake_utilities/${FILENAME}"
+                    STATUS DOWNLOAD_STATUS)
+            list(GET DOWNLOAD_STATUS 0 DOWNLOAD_CODE)
+            if (NOT ${DOWNLOAD_CODE} EQUAL 0)
+                list(GET DOWNLOAD_STATUS 1 DOWNLOAD_MESSAGE)
+                message(FATAL_ERROR "File Download failed with Error Code: ${DOWNLOAD_CODE} ${DOWNLOAD_MESSAGE}")
+            endif ()
+        endforeach ()
+
+        file(WRITE "${CMAKE_SOURCE_DIR}/.cmake_utilities/.valid" "")
+    endif ()
+
+endblock()
