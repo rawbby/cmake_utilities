9091ade05794db2337de7bb86dbb10d2e3763c24
Robert Andreas Fritsch <git@robert-fritsch.de>
2025-01-22 17:21:00 +0100

    fix shared win (hope)

--- test/shared_lib/src/source.cpp
+++ test/shared_lib/src/source.cpp
@@ -1,8 +1,8 @@
 #include <shared.h>
 #include <shared_private.h>
 
-void shared_function_a() {}
+SHARED_LIB_API void shared_function_a() {}
 
-void shared_function_b() { shared_function_a(); }
+SHARED_LIB_API void shared_function_b() { shared_function_a(); }
 
 void shared_function_d() { shared_function_c(); }
