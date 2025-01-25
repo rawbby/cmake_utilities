248d0cd21c8e7dc7ddfe62bc78dce9ec9a8d6370
Robert Andreas Fritsch <git@robert-fritsch.de>
2025-01-25 17:39:52 +0100

    update version

--- bootstrap.cmake
+++ bootstrap.cmake
@@ -1,6 +1,6 @@
 cmake_minimum_required(VERSION 3.14)
 
-set(CMAKE_UTILITIES_VERSION "main" CACHE STRING "The Version of CMakeUtilities to use")
+set(CMAKE_UTILITIES_VERSION "2.1.0" CACHE STRING "The Version of CMakeUtilities to use")
 
 block()
 
