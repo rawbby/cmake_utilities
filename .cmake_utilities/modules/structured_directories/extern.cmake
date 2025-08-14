254122b3779c918be9b4780c79248a118892827c
Robert Andreas Fritsch <git@robert-fritsch.de>
2025-08-14 15:31:57 +0200

    silence warnings on external projects and dont build tests for external projects

--- .cmake_utilities/modules/structured_directories/extern.cmake
+++ .cmake_utilities/modules/structured_directories/extern.cmake
@@ -1,5 +1,23 @@
 foreach (TARGET_DIR ${EXTERN_DIRS})
     get_filename_component(TARGET_NAME "${TARGET_DIR}" NAME)
+
+    get_property(_orig_cache_entry CACHE BUILD_TESTING PROPERTY TYPE)
+    get_property(_orig_cache_value CACHE BUILD_TESTING PROPERTY VALUE)
+    get_property(_orig_cache_doc CACHE BUILD_TESTING PROPERTY HELPSTRING)
+    set(BUILD_TESTING OFF CACHE BOOL "Temporarily disable testing for external project" FORCE)
+
+    set(_prev_SUPPRESS ${CMAKE_SUPPRESS_DEVELOPER_WARNINGS})
+    set(CMAKE_SUPPRESS_DEVELOPER_WARNINGS 1)
+
     include("${TARGET_DIR}/extern.cmake")
+
+    set(CMAKE_SUPPRESS_DEVELOPER_WARNINGS ${_prev_SUPPRESS})
+
+    if (_orig_cache_entry)
+        set(BUILD_TESTING ${_orig_cache_value} CACHE ${_orig_cache_entry} "${_orig_cache_doc}" FORCE)
+    else ()
+        unset(BUILD_TESTING CACHE)
+    endif ()
+
     message("${PROJECT_NAME} - Added External Dependency  ${TARGET_NAME}")
 endforeach ()
--- bootstrap.cmake
+++ bootstrap.cmake
@@ -1,6 +1,6 @@
 cmake_minimum_required(VERSION 3.14)
 
-set(CMAKE_UTILITIES_VERSION "v2.3.10" CACHE STRING "The Version of CMakeUtilities to use")
+set(CMAKE_UTILITIES_VERSION "v2.3.11" CACHE STRING "The Version of CMakeUtilities to use")
 
 block()
     set(BASE_URL "https://raw.githubusercontent.com/rawbby/cmake_utilities/refs/tags/${CMAKE_UTILITIES_VERSION}")
